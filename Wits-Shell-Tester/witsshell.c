863738d54bafab5691c954f8cb7c5498c2ff58a1
ManassehV7809 <104124845+ManassehV7809@users.noreply.github.com>
2023-09-05 15:28:41 +0200

    Update witsshell.c

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -9,6 +9,7 @@
 #define MAX_PATHS 10
 
 char *paths[MAX_PATHS] = {"/bin/"};  // Initial path
+char error_message[30] = "An error has occurred\n";
 int path_count = 1;
 
 bool is_builtin(char *cmd) {
@@ -19,42 +20,59 @@ bool is_builtin(char *cmd) {
 
 int main(int argc, char *argv[]) {
     pid_t child;
-    char *command[MAX_COMMANDS], *token, *lineptr = NULL; 
+    char *command[MAX_COMMANDS], *token, *lineptr = NULL;
     size_t n;
     int status;
     char *saveptr;
     size_t i;
 
+    FILE *input_source = stdin;  // Default to stdin
+    bool batch_mode = false;
+
+    if (argc == 2) {
+        batch_mode = true;
+        input_source = fopen(argv[1], "r");
+        if (!input_source) {
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            exit(1);
+        }
+    } else if (argc > 2) {
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
+
     while (1) {
-        printf("witsshell> ");
-        if (getline(&lineptr, &n, stdin) == -1) {  // EOF (e.g., from Ctrl-D) encountered
+        if (getline(&lineptr, &n, input_source) == -1) {  
+            if (batch_mode) {
+                fclose(input_source);  // Close batch file if in batch mode
+            }
             free(lineptr);
             exit(0);
         }
-        
+
         i = 0;
         token = strtok_r(lineptr, " \t\n\r", &saveptr);
-        while (i < MAX_COMMANDS - 1 && token != NULL) {  // Reserve one space for NULL termination
+        while (i < MAX_COMMANDS - 1 && token != NULL) {
             command[i] = token;
             token = strtok_r(NULL, " \t\n\r", &saveptr);
             i++;
         }
-        command[i] = NULL;  // NULL-terminate the command array
+        command[i] = NULL;
 
         if (is_builtin(command[0])) {
             if (strcmp(command[0], "exit") == 0) {
                 if (command[1] != NULL) {
-                    fprintf(stderr, "exit: Too many arguments\n");
+                    write(STDERR_FILENO, error_message, strlen(error_message));
                 } else {
                     free(lineptr);
                     exit(0);
                 }
             } else if (strcmp(command[0], "cd") == 0) {
                 if (command[1] == NULL || command[2] != NULL) {
-                    fprintf(stderr, "cd: Invalid arguments\n");
+                    write(STDERR_FILENO, error_message, strlen(error_message));
                 } else {
                     if (chdir(command[1]) != 0) {
-                        perror("cd");
+                        write(STDERR_FILENO, error_message, strlen(error_message));
                     }
                 }
             } else if (strcmp(command[0], "path") == 0) {
@@ -63,7 +81,7 @@ int main(int argc, char *argv[]) {
                     paths[path_count++] = command[j];
                 }
             }
-            continue;  // Skip the rest of the loop for built-in commands
+            continue;
         }
 
         char full_command[512];
@@ -78,28 +96,27 @@ int main(int argc, char *argv[]) {
         }
 
         if (!command_found) {
-            fprintf(stderr, "%s: Command not found\n", command[0]);
+            write(STDERR_FILENO, error_message, strlen(error_message));
             continue;
         }
 
         child = fork();
         if (child == -1) {
-            perror("fork");
+            write(STDERR_FILENO, error_message, strlen(error_message));
             free(lineptr);
             exit(EXIT_FAILURE);
         }
 
-        if (child == 0) {  // Child process
+        if (child == 0) {
             if (execv(full_command, command) == -1) {
-                perror("execv");
+                write(STDERR_FILENO, error_message, strlen(error_message));
                 exit(EXIT_FAILURE);
             }
-        } else {  // Parent process
+        } else {
             wait(&status);
         }
     }
 
-    // Shouldn't reach here but just in case
     free(lineptr);
     return 0;
 }
